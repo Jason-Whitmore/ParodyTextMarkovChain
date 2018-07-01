ea23e1739ec7dde52880f7298633bdceace0766f
Jason Whitmore <whitmoj3@wwu.edu>
2018-06-30 20:23:16 -0700

    adding files to project

---
+++ Helper.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <vector>
+#include <random>
+#include <map>
+#include <cmath>
+#include <string>
+#include <iostream>
+
+
+class Helper {
+	public:
+
+	std::vector<std::string> split();
+
+
+	private:
+
+
+};
---
+++ MarkovChain.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <vector>
+#include <random>
+#include <map>
+#include <cmath>
+#include <string>
+#include <iostream>
+
+
+class MarkovChain {
+	public:
+	MarkovChain(std::string filePath);
+
+
+	private:
+
+	int numUniqueWords;
+
+
+
+};
