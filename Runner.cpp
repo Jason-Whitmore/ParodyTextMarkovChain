92dbcd3432cc41124a9c5a87266b4d9f6aecf0a8
Jason Whitmore <whitmoj3@wwu.edu>
2018-07-04 22:20:29 -0700

    adding adjlist data

--- Runner.cpp
+++ Runner.cpp
@@ -5,11 +5,14 @@ int main() {
 	srand(NULL);
 	MarkovChain m = MarkovChain("MarkovChainData.txt", true);
 	//MarkovChain m = MarkovChain("AGOT.txt", false);
-	//m.printHighestProbability();
-	//m.writeAdjListToFile("MarkovChainData.txt");
+	
 
+	
+
+
+	std::cout << counter + 1 + n;
 	for (int i = 0; i < 500; i++) {
-		std::cout << m.generateText(1) << std::endl << std::endl;
+		//std::cout << m.generateText(1) << std::endl << std::endl;
 	}
 	
 	while(true);
